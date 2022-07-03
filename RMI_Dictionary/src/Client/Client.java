508e395314d5403795b212602be756107bc65e7c
YuelinLiu_22 <liuyuelin@YuelinMac.modem>
2022-07-04 02:30:48 +1000

    UPDATE IO Interaction and Simple method to check input

--- RMI_Dictionary/src/Client/Client.java
+++ RMI_Dictionary/src/Client/Client.java
@@ -1,11 +1,12 @@
 package Client;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 import Remote.IDictionary;
-
-import java.io.*;
 
 //handle io streams 
 public class Client {
@@ -15,13 +16,64 @@ public class Client {
             int port = Integer.parseInt(args[1]);
             try {
                 IDictionary dService = (IDictionary) Naming.lookup(String.format("rmi://%s:%d/dictionary",ipAddress,port));
-                System.out.println("Client Test:\nWord: c, Meaning: "+dService.query("c"));
+                System.out.println("\nWelcome to Online Dictionary! You can query, add or remove words!");
+                System.out.println("You should follow below instructions:\n>>>>>>>>>>>>>>>>>>>>>>");
+                System.out.println("Query: query,word\nAdd: add,word,definition\nRemove: remove,word");
+                System.out.println("For example: query,hello\n>>>>>>>>>>>>>>>>>>>>>>");
+			    Scanner scanner = new Scanner(System.in);
+			    String inputStr = null;
+                while (!(inputStr = scanner.nextLine()).equals("exit")) {
+				
+                    String received = null;
+                    if(!checkInput(inputStr)){
+                        System.out.println("Invalid Input!");
+                        continue;
+                    }
+                    String[] command = inputStr.split(",");
+                    if(command[0].equals("query")){
+                        received= dService.query(command[1]);
+                    }
+                    if(command[0].equals("add")){
+                        received = dService.add(command[1]+","+command[2]);
+                    }
+                    if(command[0].equals("remove")){
+                        received = dService.remove(command[1]);
+                    }
+                    System.out.println(received);
+                }
+                scanner.close();
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }else{
             System.out.println("Usage: Client <serverIPAddress><serverPort>");
         }
+    }
+
+    public static boolean checkInput(String inputString) {
+		String[] input = inputString.split(",");
+		boolean flag = true;
+		if(input.length<2){
+			flag = false;
+		}
+		else if(input.length == 2){
+			if(input[0].equals("query")||input[0].equals("remove")){
+				flag = true;
+			}
+			else{
+				flag = false;
+			}
+		}
+		else if(input.length ==3){
+			if(input[0].equals("add")){
+				flag = true;
+			}
+			else{
+				flag = false;
+			}
+		}
+		return flag;	
     }
 }
 
